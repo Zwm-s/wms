d384619f07c9e1dc682d2158a4aa4eb09f569f4b
zhiwomashe <13815679+zhiwomashe@user.noreply.gitee.com>
2024-03-10 17:41:52 +0800

    On 2024.3.10

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -31,25 +31,110 @@
           </el-form-item>
           <el-form-item label="密码" prop="password">
             <el-input style="width: 200px" type="password" v-model="loginForm.password"
-                      show-password autocomplete="off" size="small" @keyup.enter.native="confirm"></el-input>
+                      show-password autocomplete="off" size="small" @keyup.enter.native="login"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary" @click="confirm" :disabled="confirm_disabled">确 定</el-button>
+            <el-button type="primary" @click="login" :disabled="confirm_disabled">登 录</el-button>
+            <el-button type="info" @click="register" :disabled="confirm_disabled" style="margin-left: 55px;">注 册</el-button>
           </el-form-item>
         </el-form>
       </div>
+      <el-dialog
+          title="注 册"
+          :visible.sync="centerDialogVisible"
+          width="30%"
+          center>
+        <el-form ref="form" :rules="rules" :model="form" label-width="80px">
+          <el-form-item label="账号" prop="number">
+            <el-col :span="20">
+              <el-input v-model="form.number"></el-input>
+            </el-col>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-col :span="20">
+              <el-input v-model="form.password"></el-input>
+            </el-col>
+          </el-form-item>
+          <el-form-item label="名字" prop="name">
+            <el-col :span="20">
+              <el-input v-model="form.name"></el-input>
+            </el-col>
+          </el-form-item>
+          <el-form-item label="年龄" prop="age">
+            <el-col :span="20">
+              <el-input v-model="form.age"></el-input>
+            </el-col>
+          </el-form-item>
+          <el-form-item label="性别">
+            <el-radio-group v-model="form.sex">
+              <el-radio label="0">女</el-radio>
+              <el-radio label="1">男</el-radio>
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="电话" prop="phone">
+            <el-col :span="20">
+              <el-input v-model="form.phone"></el-input>
+            </el-col>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+        <el-button @click="centerDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="conf">确 定</el-button>
+      </span>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script>
 
-  import service from "@/axios/request";
-
-  export default {
+export default {
     name: "LogIn",
     data() {
+      let checkAge=(rule,value,callback)=>{
+        if(value>150){
+          callback(new Error('年龄输入过大'))
+        }else{
+          callback()
+        }
+      };
       return {
+        //注册表显示
+        centerDialogVisible:false,
+        //注册表单
+        form:{
+          number: '',
+          name: '',
+          password: '',
+          age: '',
+          sex: '0',
+          phone: '',
+          roleId: '2'
+        },
+        rules: {
+          phone: [
+            { required: true, message: '请输入电话号码', trigger: 'blur' },
+            { pattern: /^1[3|4|5|6|7|8|9][0-9]\d{8}$/, message: '请输入正确的电话号码', trigger: 'blur' }
+          ],
+          age: [
+            { required: true, message: '请输入年龄', trigger: 'blur' },
+            { min: 1, max: 3, message: '长度在 1 到 3 个数字', trigger: 'blur' },
+            { pattern: /^([1-9][0-9]*){1,3}$/, message: '请输入正确的年龄', trigger: 'blur' },
+            { validator: checkAge,trigger: 'blur' },
+          ],
+          password: [
+            { required: true, message: '请输入密码', trigger: 'blur' },
+            { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' }
+          ],
+          number: [
+            { required: true, message: '请输入账号', trigger: 'blur' },
+            { min: 3, max: 8, message: '长度在 3 到 8 个字符', trigger: 'blur' },
+          ],
+          name: [
+            { required: true, message: '请输入名字', trigger: 'blur' },
+            { min: 3, max: 5, message: '长度在 3 到 5 个字符', trigger: 'blur' }
+          ]
+        },
         userImage:'',
         confirm_disabled: false,
         loginForm: {
@@ -67,7 +152,34 @@
       }
     },
     methods: {
-      confirm() {
+      conf(){
+        this.$refs.form.validate((valid) => {
+          if (valid) {
+            this.$axios.post(this.$httpUrl+'/register',this.form).then(res=>res.data).then(async res =>{
+              if(res.code ===1){
+                this.$message({
+                  message: '注册成功',
+                  type: 'success'
+                });
+                this.centerDialogVisible=false;
+                this.confirm_disabled = false;
+              }else{
+                this.$message({
+                  message: '注册失败，账号已存在',
+                  type: 'error'
+                });
+              }
+            })
+          } else {
+            console.log('error submit!!');
+          }
+        });
+      },
+      register(){
+        this.confirm_disabled = true;
+        this.centerDialogVisible=true;
+      },
+      login() {
         this.confirm_disabled = true;
         this.$refs.loginForm.validate((valid) => {
           if (valid) { //valid成功为true, 失败为false//去后台验证用户名密码
